e089290c07dd608d0767d24fc8986dbdfd451a92
A-Kashif108 <asadullahkashif108@gmail.com>
2022-05-19 11:27:36 +0530

    done

--- firebase.js
+++ firebase.js
@@ -52,138 +52,7 @@ var uid="";
 const title = frm["ttl"];
 const description = frm["tsk"];
 
-popBtn.addEventListener("click", ()=>{
-  popup.classList.add("open-popup");
-  title.focus();
-});
 
-
-  btn.addEventListener("click", async ()=>{
-    const docRef = await addDoc(collection(db, uid.toString()), {
-      Title: title.value,
-      Description: description.value,
-      status:"notDone",
-      Deadline: deadline.value,
-    });
-    frm.reset();
-    popup.classList.remove("open-popup");
-      });
-      canBtn.addEventListener("click",()=>{
-        frm.reset();
-    popup.classList.remove("open-popup");
-      });
-
-
-const q = query(collection(db, uid.toString()));
-const querySnapshot = await getDocs(q);
-
-all.innerHTML = "";
-querySnapshot.forEach((doc) => {
-  const task = doc.data();
-
-  all.innerHTML += `
-  <div class="allCard">
-<h3 class="h5">${task.Title}</h3>
-<p>${task.Description}</p>
-<h4 class="h5">${task.Deadline}</h4>
-<div>
-  <button class="delBtn" id="delBtn" data-id="${doc.id}">
-  Delete
-  </button>
-  <button id="missBtn" data-id="${doc.id}">
-  Missed
-  </button>
-  <button id="doneBtn" data-id="${doc.id}">
-  Done
-  </button>
-</div>
-</div>`;
-
-});
-
-
-const comp = query(collection(db, uid.toString()), where("status", "==", "Done"));
-const completedSnapshot = await getDocs(comp);
-
-
-completed.innerHTML = "";
-completedSnapshot.forEach((doc) => {
-  const task = doc.data();
-  
-  completed.innerHTML += `
-  <div class="allCard">
-<h3 class="h5">${task.Title}</h3>
-<p>${task.Description}</p>
-<div>
-  <button class="delBtn" id="delBtn" data-id="${doc.id}">
-  Delete
-  </button>
-</div>
-</div>`;
-
-});
-const miss = query(collection(db, uid.toString()), where("status", "==", "Missed"));
-const missedSnapshot = await getDocs(miss);
-
-
-missed.innerHTML = "";
-missedSnapshot.forEach((doc) => {
-  const task = doc.data();
-  
-  missed.innerHTML += `
-  <div class="allCard">
-<h3 class="h5">${task.Title}</h3>
-<p>${task.Description}</p>
-<div>
-  <button class="delBtn" id="delBtn" data-id="${doc.id}">
-  Delete
-  </button>
-  <button id="editBtn" data-id="${doc.id}">
-  reSchedule
-  </button>
-</div>
-</div>`;
-
-});
-
-
-const btnsDone = document.querySelectorAll("#doneBtn");
-const btnsMissed = document.querySelectorAll("#missBtn");
-
-btnsDone.forEach((btn)=>
-  btn.addEventListener("click", async({target: {dataset}})=>{
-    const taskRef = doc(db, uid.toString(), dataset.id);
-  await setDoc(taskRef, { status: "Done" }, { merge: true });
-  })
-);
-
-btnsMissed.forEach((btn)=>
-  btn.addEventListener("click", async({target: {dataset}})=>{
-    const taskRef = doc(db, uid.toString(), dataset.id);
-  await setDoc(taskRef, { status: "Missed" }, { merge: true });
-  })
-);
-
-const btnsDelete = document.querySelectorAll("#delBtn");
-
-btnsDelete.forEach((butn)=>
-butn.addEventListener("click", async({target: {dataset}})=>{
-  const taskRef = doc(db, uid.toString(), dataset.id);
-  await deleteDoc(taskRef);
-}));
-const btnsReshed = document.querySelectorAll("#editBtn");
-
-btnsReshed.forEach((btn)=>
-btn.addEventListener("click", async({target: {dataset}})=>{
-  const taskRef = doc(db, uid.toString(), dataset.id);
-  const dataSnap = await getDoc(taskRef);
-  const data = dataSnap.data();
-  popup.classList.add("open-popup");
-  title.value = data.Title;
-  description.value = data.Description;
-  await deleteDoc(taskRef);
-  
-}));
 
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";;
 
@@ -196,6 +65,147 @@ onAuthStateChanged(auth, (user) => {
     logout.classList.add("open-logout");
     popBtn.classList.add("open-add");
     uid = user.uid;
+
+    popBtn.addEventListener("click", ()=>{
+      popup.classList.add("open-popup");
+      title.focus();
+    });
+    
+    
+      btn.addEventListener("click", async ()=>{
+        const docRef = await addDoc(collection(db, uid.toString()), {
+          Title: title.value,
+          Description: description.value,
+          status:"notDone",
+          Deadline: deadline.value,
+        });
+        frm.reset();
+        popup.classList.remove("open-popup");
+          });
+          canBtn.addEventListener("click",()=>{
+            frm.reset();
+        popup.classList.remove("open-popup");
+          });
+    
+    
+    const q = query(collection(db, uid.toString()));
+    const querySnapshot = await getDocs(q);
+    
+    all.innerHTML = "";
+    querySnapshot.forEach((doc) => {
+      const task = doc.data();
+    
+      all.innerHTML += `
+      <div class="allCard">
+    <h3 class="h5">${task.Title}</h3>
+    <p>${task.Description}</p>
+    <h4 class="h5">${task.Deadline}</h4>
+    <div>
+      <button class="delBtn" id="delBtn" data-id="${doc.id}">
+      Delete
+      </button>
+      <button id="missBtn" data-id="${doc.id}">
+      Missed
+      </button>
+      <button id="doneBtn" data-id="${doc.id}">
+      Done
+      </button>
+    </div>
+    </div>`;
+    
+    });
+    
+    
+    const comp = query(collection(db, uid.toString()), where("status", "==", "Done"));
+    const completedSnapshot = await getDocs(comp);
+    
+    
+    completed.innerHTML = "";
+    completedSnapshot.forEach((doc) => {
+      const task = doc.data();
+      
+      completed.innerHTML += `
+      <div class="allCard">
+    <h3 class="h5">${task.Title}</h3>
+    <p>${task.Description}</p>
+    <div>
+      <button class="delBtn" id="delBtn" data-id="${doc.id}">
+      Delete
+      </button>
+    </div>
+    </div>`;
+    
+    });
+    const miss = query(collection(db, uid.toString()), where("status", "==", "Missed"));
+    const missedSnapshot = await getDocs(miss);
+    
+    
+    missed.innerHTML = "";
+    missedSnapshot.forEach((doc) => {
+      const task = doc.data();
+      
+      missed.innerHTML += `
+      <div class="allCard">
+    <h3 class="h5">${task.Title}</h3>
+    <p>${task.Description}</p>
+    <div>
+      <button class="delBtn" id="delBtn" data-id="${doc.id}">
+      Delete
+      </button>
+      <button id="editBtn" data-id="${doc.id}">
+      reSchedule
+      </button>
+    </div>
+    </div>`;
+    
+    });
+    
+    
+    const btnsDone = document.querySelectorAll("#doneBtn");
+    const btnsMissed = document.querySelectorAll("#missBtn");
+    
+    btnsDone.forEach((btn)=>
+      btn.addEventListener("click", async({target: {dataset}})=>{
+        const taskRef = doc(db, uid.toString(), dataset.id);
+      await setDoc(taskRef, { status: "Done" }, { merge: true });
+      })
+    );
+    
+    btnsMissed.forEach((btn)=>
+      btn.addEventListener("click", async({target: {dataset}})=>{
+        const taskRef = doc(db, uid.toString(), dataset.id);
+      await setDoc(taskRef, { status: "Missed" }, { merge: true });
+      })
+    );
+    
+    const btnsDelete = document.querySelectorAll("#delBtn");
+    
+    btnsDelete.forEach((butn)=>
+    butn.addEventListener("click", async({target: {dataset}})=>{
+      const taskRef = doc(db, uid.toString(), dataset.id);
+      await deleteDoc(taskRef);
+    }));
+    const btnsReshed = document.querySelectorAll("#editBtn");
+    
+    btnsReshed.forEach((btn)=>
+    btn.addEventListener("click", async({target: {dataset}})=>{
+      const taskRef = doc(db, uid.toString(), dataset.id);
+      const dataSnap = await getDoc(taskRef);
+      const data = dataSnap.data();
+      popup.classList.add("open-popup");
+      title.value = data.Title;
+      description.value = data.Description;
+      await deleteDoc(taskRef);
+      
+    }));
+
+
+
+
+
+
+
+
     // ...
   } else {
     // User is signed out
